966db0c8f6c775cec93575c078e96b66401892e7
Valegox <valentin.gegoux@epitech.eu>
2025-05-07 00:40:49 +0900

    Try fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import config from "../next.config";
+import Head from 'next/head';
+
 
 const App = ({ Component, pageProps }) => {
+  const basePath = config.basePath ? config.basePath : "";
+
   return (
-    <ThemeProvider
-      defaultTheme="dark"
-      enableSystem={false}
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
+      </Head>
+
+      <ThemeProvider
+        defaultTheme="dark"
+        enableSystem={false}
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 };
 
